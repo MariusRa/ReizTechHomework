500b23840d007c96364e3d57bccf5d47841e86b4
Marius Raudys <marius.raudys@gmail.com>
2022-10-12 17:01:02 +0300

    addedMyTasks

---
+++ DirectoryWithSubdirectories/DirectoryWithSubdirectories/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryWithSubdirectories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string root = @""; // between "" enter path where new directories will be created
+            CreateDirectories(root);
+
+            Console.WriteLine("Press any key to continous");
+            Console.ReadLine();
+            
+            Console.WriteLine("List of created folder with subfolders:");
+            string rootPath = $@"{root}\Folder";
+            var newDivs = FindDirectories(rootPath);
+            
+            var folderDepth = CountDirectoriesDepth(newDivs, rootPath);
+
+            Console.WriteLine();
+            Console.WriteLine($"Created Folder has {newDivs.Length} subfolders divided in {folderDepth} levels.");
+
+            Console.ReadLine();
+        }
+
+        // Create new directories
+        private static void CreateDirectories(string root)
+        {
+            List<string> directories = new List<string>();
+
+            directories.Add(new string(@$"{root}\Folder"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder1"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder1\SubSubFolder1"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder1"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder1\SubSubSubFolder1"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder2"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder2\SubSubSubFolder1"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder2\SubSubSubFolder1\SubSubSubSubFolder1"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder2\SubSubSubFolder2"));
+            directories.Add(new string(@$"{root}\Folder\Subfolder2\SubSubFolder3"));
+
+            foreach (var directory in directories)
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+
+        // Find directories
+        private static string[] FindDirectories(string path)
+        {
+            string []dirs = {};
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                dirs = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+                Console.WriteLine(dir);
+            }
+                        
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                FindDirectories(dir);
+            }
+
+            return dirs;
+        }
+
+        // Count structure depth of directory
+        private static int CountDirectoriesDepth(string[] dirs, string basePath)
+        {
+            var depth = new List<int>();
+
+            foreach (var dir in dirs)
+            {
+                depth.Add(dir.Count(x => x == '\\'));
+            }
+                                  
+            return depth.Max() - basePath.Count(x => x == '\\') + 1;
+        } 
+    }
+}
---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,66 @@
+﻿// Calculate angles between arrows
+
+static decimal CountAngle(int hour, int minutes)
+{
+    var hourAngle = (hour * 360 / 12 + (minutes / 60m) * 360 / 12m); // each hour mark is 30 degrees. 
+    var minutesAngle = 360 / 60 * minutes; // each minute mark is 6 degrees.
+
+    var angleBetween = Decimal.Round(Math.Abs(hourAngle - minutesAngle), 2);
+
+    return angleBetween;
+}
+
+//Declare and validate inputs 
+
+int hour;
+int minutes;
+
+Console.WriteLine("Enter hour");
+    try
+    {
+        hour = Convert.ToInt32(Console.ReadLine());
+        if (hour < 0 || hour > 12)
+        {
+            Console.WriteLine("Wrong input. Reenter hour!");
+            hour = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Wrong input. Reenter hour!");
+        hour = Convert.ToInt32(Console.ReadLine());
+    }
+  
+Console.WriteLine("Enter minutes");
+    try
+    {
+        minutes = Convert.ToInt32(Console.ReadLine());
+        if (minutes < 0 || minutes > 60)
+        {
+            Console.WriteLine("Wrong input. Reenter minutes!");
+            minutes = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Wrong input. Reenter minutes!");
+        minutes = Convert.ToInt32(Console.ReadLine());
+    }
+    
+        if (hour == 12)
+        {
+            hour = 0;
+        }
+
+        if (minutes == 60)
+        {
+            minutes = 0;
+            hour += 1;
+            if (hour > 12)
+            {
+                hour = hour - 12;
+            }
+        }
+
+Console.WriteLine($"Entered time is {hour}:{minutes}.");
+Console.WriteLine($"Angle between arrows are: {CountAngle(hour, minutes)} degrees.");
